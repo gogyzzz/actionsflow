e7725eb446d950788942e654f51b31e45039a5b3
haeyong <gogyzzz@gmail.com>
2023-02-18 10:40:40 +0900

    Create fear_and_greed.py

---
+++ src/fear_and_greed.py
@@ -0,0 +1,22 @@
+import fear_and_greed
+import os
+
+fgx = fear_and_greed.get()
+
+channel_id = "#알림"
+message = f"SPX sentiment: {fgx.description} ({round(fgx.value, 2)})" 
+SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
+                                                
+                                                
+try:
+    # Initialize a Slack WebClient instance with the bot token
+    client = WebClient(token=SLACK_BOT_TOKEN)
+
+    response = client.chat_postMessage(
+        channel=channel_id,
+        text=message,
+    )
+    thread_ts = response["ts"]
+
+except SlackApiError as e:
+    print("Error uploading file to Slack: {}".format(e))
